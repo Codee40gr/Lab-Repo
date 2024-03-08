07ad23101cc07023d76b23b242d0baf8f8b913c2
Mahmut İsmail Öztürk <60552767+Codee40gr@users.noreply.github.com>
2024-03-08 15:30:11 +0300

    Add files via upload

---
+++ FindPrime.java
@@ -0,0 +1,23 @@
+package section2.lab3;
+
+@SuppressWarnings("unused")
+public class FindPrime{
+    public static void main(String[] args) {
+        
+        int given = 30;
+
+        for (int number = 2; number < given; number++){
+            boolean isPrime = true;
+
+            for (int divisor = 2; divisor < number; divisor++){
+                if (number % divisor == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime){
+                System.out.println(number);
+            }
+        }
+    }
+}
